cf4891f222cf431f3af49b35120b6aacbfd1f377
ben1hop <https://github.com/ben1hop>
2020-04-16 12:57:22 +0200

    004 - Creating & Deleting Landscapes

---
+++ Source/Source/Source/SourceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SourceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOURCE_API ASourceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
